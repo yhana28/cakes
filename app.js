1fe109ed3d09b3a74c76c187b86abc686017022c
Darwin Rosales <dawen22292@gmail.com>
2026-04-15 22:31:32 +0800

    update

--- app.js
+++ app.js
@@ -55,11 +55,3 @@ var swiper = new Swiper(".mySwiper", {
     },
     loop: true
 });
-
-function toggleMenu() {
-    const menuToggle = document.querySelector('.toggle');
-    const navigation = document.querySelector('.navigation');
-
-    menuToggle.classList.toggle('active');
-    navigation.classList.toggle('active');
-}
